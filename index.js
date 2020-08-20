af3faf597b6831745553e31fbe51194e0d9600b6
Mathias Maier <mathias.maier@tidal.com>
2020-08-20 15:24:41 +0200

    remove all copied rules and use airbnb as an import

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 module.exports = {
-  extends: [
-    './rules/best-practices',
-    './rules/errors',
-    './rules/es6',
-    './rules/imports',
-    './rules/node',
-    './rules/strict',
-    './rules/variables',
-    './rules/style',
-  ].map(require.resolve),
+  extends: ['airbnb'],
+  // .map(require.resolve)
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
